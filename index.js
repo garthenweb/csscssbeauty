b7a8e79eac9f8ed44dc8d79a1aa29e4ade7d822a
Jannick Garthen <jannick.garthen@gmail.com>
2013-09-29 23:29:14 +0200

    fixes timing and empty file problems

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-var argv    = require('optimist')
+var argv = require('optimist')
 	.boolean('v')
 	.boolean('verbose')
 	.boolean('color')
@@ -21,30 +21,46 @@ var sockets = require('./socket')(server);
 // object store
 var store = {};
 
+// update the data
+var updateData = (function () {
+
+	var timesReloaded = 0;
+
+	return function(err, data) {
+		if(err) {
+			throw err;
+		}
+
+		store.data = data ? JSON.parse(data) : [];
+		sockets.emit('update', {trigger: true});
+
+		if(timesReloaded === 0) {
+			// open browser or notify the user to open the browser
+			if(argv.browser === 'false') {
+				console.log('Connect to http://localhost:' + server.address().port + ' to see the result');
+			} else {
+				open('http://localhost:' + server.address().port, argv.browser);
+			}
+		}
+
+		timesReloaded++;
+
+	};
+
+})();
+
 // wrapper for csscss generator
-function runCSSCSS() {
+var runCSSCSS = (function() {
+
 	var args = (argv._)
 		.concat(require('./csscss-args')(argv))
 		.concat(['--json']);
 
-	csscss(args, updateData);
-}
+	return function() {
+		csscss(args, updateData);
+	};
 
-// update the data
-function updateData(err, data) {
-	if(err) {
-		throw err;
-	}
-
-	store.data = JSON.parse(data);
-	sockets.emit('update', {trigger: true});
-}
-
-// set file watcher
-watch(argv._, runCSSCSS);
-
-// initial generate data
-runCSSCSS();
+})();
 
 // routing
 app.get('/', function(req, res) {
@@ -53,8 +69,8 @@ app.get('/', function(req, res) {
 
 // start server on given port and open the browser
 server.listen(process.env.PORT || argv.port);
-if(argv.browser === 'false') {
-	console.log('Connect to http://localhost:' + server.address().port + ' to see the result');
-} else {
-	open('http://localhost:' + server.address().port, argv.browser);
-}
+
+// initial generate data
+runCSSCSS();
+// activate file watcher
+watch(argv._, runCSSCSS);
